98acbf213f2ad8d0c0a8071f0eb5b4801138aa54
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-09-26 18:57:31 +0200

    kan nu update en ordrer

--- server/service/Services/OrderService.cs
+++ server/service/Services/OrderService.cs
@@ -11,6 +11,7 @@ namespace service.Services
         Task<OrderDto> CreateOrderAsync(OrderDto createOrderDto);
         Task<OrderDto?> GetOrderByIdAsync(int id);
         Task<List<OrderDto>> GetOrdersByCustomerIdAsync(int customerId);
+        Task<OrderDto?> UpdateOrderByIdAsync(int id, OrderDto updateOrderDto);
     }
 
     public class OrderService : IOrderService
@@ -39,7 +40,7 @@ namespace service.Services
             var order = createOrderDto.ToOrder();
             await _context.Orders.AddAsync(order);
             await _context.SaveChangesAsync();
-            _logger.LogInformation("Order created successfully"); // Logging statement
+            _logger.LogInformation("Order created successfully"); 
             return new OrderDto().FromEntity(order);
         }
         
@@ -53,6 +54,29 @@ namespace service.Services
         {
             var orders = await _context.Orders.Where(o => o.CustomerId == customerId).ToListAsync();
             return orders.Select(order => new OrderDto().FromEntity(order)).ToList();
+        }
+
+        public async Task<OrderDto?> UpdateOrderByIdAsync(int id, OrderDto updateOrderDto)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return null;
+            }
+
+            // Update order properties
+            order.CustomerId = updateOrderDto.CustomerId;
+            order.OrderDate = updateOrderDto.OrderDate;
+            order.TotalAmount = updateOrderDto.TotalAmount;
+            order.DeliveryDate = updateOrderDto.DeliveryDate;
+            order.Status = updateOrderDto.Status;
+
+           
+            await _updateOrderValidator.ValidateAndThrowAsync(updateOrderDto);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Order was succesfully updated");
+            
+            return new OrderDto().FromEntity(order);
         }
     }
 }
