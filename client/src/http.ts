d23150a3a5c585f4053566c0452ae4f9eb8ed883
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-10-03 15:59:59 +0200

    ny branch

--- client/src/http.ts
+++ client/src/http.ts
@@ -5,3 +5,11 @@ export const baseUrl = 'http://localhost:5139';
 export const http = new Api({
     baseURL: baseUrl
 });
+
+// http.instance.interceptors.response.use( r => {
+//     if(r.status == 401) {
+//
+//     }
+// })
+
+//TODO error messages here
