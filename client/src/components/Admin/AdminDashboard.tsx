dfb161202540b8d29183f2e71a1e680092b4c117
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-10-06 18:47:03 +0200

    Styleing

--- client/src/components/Admin/AdminDashboard.tsx
+++ client/src/components/Admin/AdminDashboard.tsx
@@ -4,7 +4,6 @@ import Menu from "./Menu.tsx";
 const AdminDashboard = () => {
     return (
         <>
-
         <div className="flex flex-col justify-center items-center mt-12">
             <h1 className="text-4xl">Admin Dashboard</h1>
             <p className="text-2xl">Welcome, Admin! Here you can manage the application.</p>
