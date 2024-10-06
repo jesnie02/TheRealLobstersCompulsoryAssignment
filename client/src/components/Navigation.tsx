0f6707b3280778fe21189d551099b8e694f55ef1
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-10-06 18:23:05 +0200

    Lobster Logo

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -6,16 +6,16 @@ import HomeLogo from "./Utilities/HomeLogo.tsx";
 const Navigation = () => {
     return (
         <nav className="navigation fixed top-0 left-0 right-0 flex justify-between items-center bg-white text-black p-3 shadow-lg z-50">
-            <div className="flex items-center">
-                <div>Logo</div>
-                <HomeLogo />
-            </div>
-            <div className="profile-center">
-                <ProfileLogo />
-            </div>
-            <div className="flex items-center">
-                <CartLogo />
-                <ThemeSwitcher className="theme-switcher" />
+            <div className="flex items-center flex-1">
+                <div>
+                    <img src="/assets/LobsterPaperLogo.png" alt="Lobster Paper Logo" className="h-40 w-auto"/>
+                </div>
+                <div className="flex justify-between w-full">
+                    <HomeLogo/>
+                    <ProfileLogo/>
+                    <CartLogo/>
+                    <ThemeSwitcher className="theme-switcher"/>
+                </div>
             </div>
         </nav>
     );
